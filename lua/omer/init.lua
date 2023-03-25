df8d5b50eca25299fd563c496dd163794291377d
omer-vishlitzky <omre.vishlitzky@gmail.com>
2023-03-25 16:53:45 +0300

    wip telescope config

--- lua/omer/init.lua
+++ lua/omer/init.lua
@@ -1,1 +1,2 @@
 require("omer.set")
+require("omer.remap")
--- lua/omer/packer.lua
+++ lua/omer/packer.lua
@@ -7,7 +7,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use 'ellisonleao/gruvbox.nvim'
-    use 'nvim-lua/plenary.nvim'
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { {'nvim-lua/plenary.nvim'} } }
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 end)
