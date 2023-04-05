271a023ca5605aacb01a9662839b709c187720fe
omer-vishlitzky <omre.vishlitzky@gmail.com>
2023-04-05 04:55:31 +0300

    added trouble

--- lua/omer/packer.lua
+++ lua/omer/packer.lua
@@ -22,5 +22,17 @@ return require('packer').startup(function(use)
                 -- refer to the configuration section below
             }
         end
-}
+    }
+    -- Lua
+    use {
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
