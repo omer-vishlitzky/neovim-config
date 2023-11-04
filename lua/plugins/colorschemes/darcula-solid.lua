bc610f45ea79ff81485744d51f70e45b79fa5482
omer-vishlitzky <omer.vishlitzky@gmail.com>
2023-11-04 15:31:58 +0200

    wip

--- lua/plugins/colorschemes/darcula-solid.lua
+++ lua/plugins/colorschemes/darcula-solid.lua
@@ -1,7 +1,7 @@
 return {
-    'briones-gabriel/darcula-solid.nvim',
-    dependencies = { "rktjmp/lush.nvim" },
-    config = function()
+  'briones-gabriel/darcula-solid.nvim',
+  dependencies = { "rktjmp/lush.nvim" },
+  config = function()
 
-    end
+  end
 }
--- lua/plugins/colorschemes/github-theme.lua
+++ lua/plugins/colorschemes/github-theme.lua
@@ -1,19 +1,19 @@
 return {
-    'projekt0n/github-nvim-theme',
-    config = function()
-        require('github-theme').setup({
-            options = {
-                inverse = { -- Inverse highlight for different types
-                    match_paren = false,
-                    visual = true,
-                    search = true,
-                },
-                styles = {
-                    comments = 'italic',
-                    keywords = 'bold',
-                    types = 'italic,bold',
-                }
-            }
-        })
-    end
+  'projekt0n/github-nvim-theme',
+  config = function()
+    require('github-theme').setup({
+      options = {
+        inverse = { -- Inverse highlight for different types
+          match_paren = false,
+          visual = true,
+          search = true,
+        },
+        styles = {
+          comments = 'italic',
+          keywords = 'bold',
+          types = 'italic,bold',
+        }
+      }
+    })
+  end
 }
