0c2882cdcfa297225943f2901fdd68f4af2fdb6d
omer-vishlitzky <omer.vishlitzky@gmail.com>
2023-11-10 23:35:04 +0200

    illuminate disabled, telescope-ui installed

--- lua/plugins/general/telescope.lua
+++ lua/plugins/general/telescope.lua
@@ -35,8 +35,40 @@ return {
       --       refer to the README for telescope-fzf-native for more instructions.
       build = 'make',
       cond = function()
-          return vim.fn.executable 'make' == 1
+        return vim.fn.executable 'make' == 1
       end
     },
+    {
+      'nvim-telescope/telescope-ui-select.nvim',
+      -- This is your opts table
+      config = function()
+        require("telescope").setup {
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown {
+                -- even more opts
+              }
+
+              -- pseudo code / specification for writing custom displays, like the one
+              -- for "codeactions"
+              -- specific_opts = {
+              --   [kind] = {
+              --     make_indexed = function(items) -> indexed_items, width,
+              --     make_displayer = function(widths) -> displayer
+              --     make_display = function(displayer) -> function(e)
+              --     make_ordinal = function(e) -> string
+              --   },
+              --   -- for example to disable the custom builtin "codeactions" display
+              --      do the following
+              --   codeactions = false,
+              -- }
+            }
+          }
+        }
+        -- To get ui-select loaded and working with telescope, you need to call
+        -- load_extension, somewhere after setup function:
+        require("telescope").load_extension("ui-select")
+      end
+    }
   },
 }
--- lua/plugins/general/vim-illuminate.lua
+++ lua/plugins/general/vim-illuminate.lua
@@ -1,8 +1,30 @@
 return {
   "RRethy/vim-illuminate",
   lazy = false,
+  enabled = false,
   config = function()
     -- default configuration
     require('illuminate').configure({})
+    vim.api.nvim_create_augroup("illuminate_autogrp", { clear = true })
+    vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+      group = "illuminate_autogrp",
+      pattern = "*",
+      callback = function()
+        vim.api.nvim_command('highlight clear CursorLine')
+        vim.api.nvim_command('highlight CursorLine guibg=#3c3836')
+        vim.api.nvim_command('highlight clear IlluminatedWordText')
+        vim.api.nvim_command('highlight clear IlluminatedWordRead')
+        vim.api.nvim_command('highlight clear IlluminatedWordWrite')
+        vim.api.nvim_command('highlight clear LspReferenceText')
+        vim.api.nvim_command('highlight clear LspReferenceRead')
+        vim.api.nvim_command('highlight clear LspReferenceWrite')
+        vim.api.nvim_command('highlight link IlluminatedWordText LspReferenceText')
+        vim.api.nvim_command('highlight link IlluminatedWordRead LspReferenceRead')
+        vim.api.nvim_command('highlight link IlluminatedWordWrite LspReferenceWrite')
+        vim.api.nvim_command('highlight link LspReferenceText CursorLine')
+        vim.api.nvim_command('highlight link LspReferenceRead CursorLine')
+        vim.api.nvim_command('highlight link LspReferenceWrite CursorLine')
+      end
+    })
   end
 }
