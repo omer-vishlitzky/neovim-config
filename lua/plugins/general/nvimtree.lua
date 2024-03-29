27508c52b63633ee2868250b88fab2e2283c7d0f
omer-vishlitzky <omer.vishlitzky@gmail.com>
2024-03-29 19:37:46 +0300

    wip

--- lua/plugins/general/nvimtree.lua
+++ lua/plugins/general/nvimtree.lua
@@ -3,16 +3,16 @@ vim.g.loaded_netrwPlugin = 1
 
 return {
   'nvim-tree/nvim-tree.lua',
+  lazy = true,
+  init = function()
+--      vim.notify("nvimtree executed at startup")
+      vim.notify("nvimtree executed at startup")
+  end,
   keys = {
     { "<C-n>", "<cmd>NvimTreeToggle<cr>", desc = "NvimTreeToggle" },
   },
-  config = function()
-    require("nvim-tree").setup()
-  end,
+  opts = {},
   dependencies = {
-    {
-      'nvim-tree/nvim-web-devicons',
-      lazy = true
-    }
+    { 'nvim-tree/nvim-web-devicons', lazy = true },
   }
 }
