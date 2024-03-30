14df02801b42a104f895a259c9588c765108841d
omer-vishlitzky <omer.vishlitzky@gmail.com>
2024-03-30 09:59:51 +0300

    wip

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -60,36 +60,40 @@ return { -- LSP Configuration & Plugins
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { buffer = event.buf, desc = '[G]oto [D]efition'})
+        vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions,
+          { buffer = event.buf, desc = '[G]oto [D]efition' })
 
         -- Find references for the word under your cursor.
-        vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { buffer = event.buf, desc = '[G]oto [R]eferences'})
+        vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references,
+          { buffer = event.buf, desc = '[G]oto [R]eferences' })
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations, { buffer = event.buf, desc = '[G]oto [I]mplementation'})
+        vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations,
+          { buffer = event.buf, desc = '[G]oto [I]mplementation' })
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        vim.keymap.set('n', '<leader>td', require('telescope.builtin').lsp_type_definitions, { buffer = event.buf, desc = 'Type [D]efinition'})
+        vim.keymap.set('n', '<leader>td', require('telescope.builtin').lsp_type_definitions,
+          { buffer = event.buf, desc = 'Type [D]efinition' })
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = event.buf, desc = '[R]e[n]ame'})
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = event.buf, desc = '[R]e[n]ame' })
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = event.buf, desc = '[C]ode [A]ction'})
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = event.buf, desc = '[C]ode [A]ction' })
 
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap.
-        vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, { buffer = event.buf, desc = 'Hover Documentation'})
+        vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, { buffer = event.buf, desc = 'Hover Documentation' })
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
-        vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, { buffer = event.buf, desc = '[G]oto [D]eclaration'})
-        vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { buffer = event.buf, desc = '[L]sp [F]ormat'})
+        vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, { buffer = event.buf, desc = '[G]oto [D]eclaration' })
+        vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { buffer = event.buf, desc = '[L]sp [F]ormat' })
 
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
@@ -178,7 +182,7 @@ return { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'stylua',   -- Used to format Lua code
+      'stylua',                                -- Used to format Lua code
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
