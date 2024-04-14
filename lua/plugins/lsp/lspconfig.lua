37c89a60f61871321cb1b1c102a4ecaf244e481e
omer <omer.vishlitzky@gmail.com>
2024-04-14 17:18:40 +0300

    wip

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -54,7 +54,6 @@ return { -- LSP Configuration & Plugins
           },
         },
       },
-      yamlls = {},
     }
     require('mason-lspconfig').setup {
       ensure_installed = {
@@ -62,14 +61,10 @@ return { -- LSP Configuration & Plugins
         "lua_ls",
         "gopls",
         "clangd",
-        -- "yamlls",
       },
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
