d9c8badf7638ab76653945845a46ea89e50790ea
omer <omer.vishlitzky@gmail.com>
2024-06-01 17:37:12 +0300

    wip

--- lua/plugins/general/lualine.lua
+++ lua/plugins/general/lualine.lua
@@ -1,5 +1,15 @@
 return {
   'nvim-lualine/lualine.nvim',
-  opts = {},
+  opts = {
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          file_status = true, -- displays file status (readonly status, modified status)
+          path = 2        -- 0 = just filename, 1 = relative path, 2 = absolute path
+        }
+      }
+    }
+  },
   event = "VeryLazy",
 }
