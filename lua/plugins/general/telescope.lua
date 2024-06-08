26a6c35b892306a08e463ce4f4d543d777506579
omer <omer.vishlitzky@gmail.com>
2024-06-08 21:00:29 +0300

    Telescope cleanup

--- lua/plugins/general/telescope.lua
+++ lua/plugins/general/telescope.lua
@@ -7,6 +7,32 @@ return {
   cmd = {
     "Telescope"
   },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end
+    },
+    {
+      'nvim-telescope/telescope-ui-select.nvim',
+      config = function()
+        local telescope = require("telescope")
+        telescope.setup {
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown {
+
+              }
+            }
+          }
+        }
+        telescope.load_extension("ui-select")
+      end
+    }
+  },
   config = function()
     require('telescope').setup {
       defaults = {
@@ -31,57 +57,10 @@ return {
     vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[F]ind [B]uffers' })
     vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
     vim.keymap.set('n', '<leader>fr', require('telescope.builtin').lsp_references, { desc = '[F]ind [R]eferences' })
-    vim.keymap.set('n', '<leader>fi', require('telescope.builtin').lsp_implementations, { desc = '[F]ind [I]mplementations' })
+    vim.keymap.set('n', '<leader>fi', require('telescope.builtin').lsp_implementations,
+      { desc = '[F]ind [I]mplementations' })
     vim.keymap.set('n', '<leader>fk', require('telescope.builtin').keymaps, { desc = '[F]ind [K]eymaps' })
     vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp tags' })
     vim.keymap.set('n', '<leader>fs', require('telescope.builtin').git_status, { desc = '[F]ind Git [S]tatus' })
   end,
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-    -- Only load if `make` is available. Make sure you have the system
-    -- requirements installed.
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      -- NOTE: If you are having trouble with this installation,
-      --       refer to the README for telescope-fzf-native for more instructions.
-      build = 'make',
-      cond = function()
-        return vim.fn.executable 'make' == 1
-      end
-    },
-    {
-      'nvim-telescope/telescope-ui-select.nvim',
-      -- This is your opts table
-      config = function()
-        local telescope = require("telescope")
-        telescope.setup {
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown {
-                -- even more opts
-              }
-
-              -- pseudo code / specification for writing custom displays, like the one
-              -- for "codeactions"
-              -- specific_opts = {
-              --   [kind] = {
-              --     make_indexed = function(items) -> indexed_items, width,
-              --     make_displayer = function(widths) -> displayer
-              --     make_display = function(displayer) -> function(e)
-              --     make_ordinal = function(e) -> string
-              --   },
-              --   -- for example to disable the custom builtin "codeactions" display
-              --      do the following
-              --   codeactions = false,
-              -- }
-            }
-          }
-        }
-        -- To get ui-select loaded and working with telescope, you need to call
-        -- load_extension, somewhere after setup function:
-        telescope.load_extension("ui-select")
-      end
-    }
-  },
 }
