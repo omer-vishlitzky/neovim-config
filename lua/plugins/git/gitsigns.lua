23b2f6512154304e4c92648349ee5818a4803aa9
omer <omer.vishlitzky@gmail.com>
2024-06-08 21:37:16 +0300

    Gitsigns keymaps

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -1,9 +1,7 @@
 return {
-  -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   event = "VeryLazy",
   opts = {
-    -- See `:help gitsigns.txt`
     signs = {
       add = { text = '+' },
       change = { text = '~' },
@@ -12,11 +10,16 @@ return {
       changedelete = { text = '~' },
     },
     on_attach = function(bufnr)
-      -- vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
-
-      vim.api.nvim_set_keymap("n", "<leader>hs", "<CMD>Gitsigns stage_hunk<CR>", { desc = "[H]unk [S]tage" })
-      vim.api.nvim_set_keymap("n", "<leader>hr", "<CMD>Gitsigns reset_hunk<CR>", { desc = "[H]unk [R]eset" })
-      vim.api.nvim_set_keymap("n", "<leader>hp", "<CMD>Gitsigns preview_hunk<CR>", { desc = "[H]unk [P]review" })
+      local gitsigns = require("gitsigns")
+      vim.keymap.set("n", "<leader>hs", "<CMD>Gitsigns stage_hunk<CR>", { desc = "[H]unk [S]tage" })
+      vim.keymap.set("n", "<leader>hr", "<CMD>Gitsigns reset_hunk<CR>", { desc = "[H]unk [R]eset" })
+      vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+      vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+      vim.keymap.set("n", "<leader>bs", "<CMD>Gitsigns stage_buffer<CR>", { desc = "[B]uffer [S]tage" })
+      vim.keymap.set("n", "<leader>br", "<CMD>Gitsigns reset_buffer<CR>", { desc = "[B]uffer [R]eset" })
+      vim.keymap.set('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "[T]oggle git [B]lame"})
+      vim.keymap.set('n', '<leader>hd', gitsigns.diffthis, { desc = "[H]unk [D]iff"})
+      vim.keymap.set("n", "<leader>hp", "<CMD>Gitsigns preview_hunk<CR>", { desc = "[H]unk [P]review" })
       -- don't override the built-in and fugitive keymaps
       local gs = package.loaded.gitsigns
       vim.keymap.set({ 'n', 'v' }, ']c', function()
