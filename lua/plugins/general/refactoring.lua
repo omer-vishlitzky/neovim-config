444db9a6d2f6de75b960d1aac3d427c72264f79b
omer <omer.vishlitzky@gmail.com>
2024-06-09 01:32:48 +0300

    changes in refactoring plugin

--- lua/plugins/general/refactoring.lua
+++ lua/plugins/general/refactoring.lua
@@ -9,19 +9,22 @@ return {
   },
   config = function()
     require('refactoring').setup({
+      prompt_func_return_type = {
+        go = true,
+        c = true,
+        python = true,
+      },
+      prompt_func_param_type = {
+        go = true,
+        c = true,
+        python = true,
+      },
       show_success_message = true,
       below = true,
     })
     vim.keymap.set("v", "<leader>re", ":Refactor extract ")
-    vim.keymap.set("v", "<leader>rf", ":Refactor extract_to_file ")
-
     vim.keymap.set("v", "<leader>rv", ":Refactor extract_var ")
-
     vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
-
     vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
-
-    vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
-    vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
   end,
 }
